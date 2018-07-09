67cee9965314d86cd56607179f0cd4ce096cddcf
dreamerwu <haowbdreamer@gmail.com>
2018-07-08 20:27:52 -0400

    Update ggpubr.R

--- ggpubr.R
+++ ggpubr.R
@@ -1,7 +1,14 @@
 #reference: https://www.r-bloggers.com/add-p-values-and-significance-levels-to-ggplots/
 library("ggpubr")
+
+#plot point and calculate t.test
 p=ggboxplot(data,x="Gene",y="TEAD1",add="jitter")
 p+stat_compare_means(method="anova/t.test")
 compare_means(TEAD1~Gene,data,method="t.test") #calculate multiple group statistics
+
+#plot point-line
+ggline(data,x="x axis",y="y axis",color="group",add="mean_se")
+
+
 
 
